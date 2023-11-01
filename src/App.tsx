63640725e97703454cb79a90f6e2114750ef32d5
joshuadazar <joshuadazar@gmail.com>
2023-11-01 08:31:38 -0500

    feature/add-validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,82 @@
 import * as React from 'react';
 import IconMail from './assets/IconMail';
+import { useState } from 'react';
 import './style.scss';
 
 export default function App() {
+  const [emailField, setEmailField] = useState('');
+  const [passwordField, setPasswordField] = useState('');
+
+  const cta = document.querySelector('.cta');
+  const validateFields = (e) => {
+    const form = document.forms['login'];
+    e.preventDefault();
+
+    console.log(form.elements.password, 'miau');
+
+    e.target.name == 'email' && setEmailField(e.target.value);
+    e.target.name == 'password1' && setPasswordField(e.target.value);
+    if (validatePassword(e.target)) {
+      setPasswordField(e.target.value);
+      cta.removeAttribute('disabled');
+    } else {
+      form.elements.btn.setAttribute('disabled', true);
+      form.elements.email.value = 'miaii';
+    }
+  };
+
+  const validatePassword = (e) => {
+    if (e.name === 'password2' && e.value === passwordField) {
+      return true;
+    }
+  };
+
   return (
     <div className="container">
-      <form action="" className="form">
-      <h1 className="form-title">Create new Account</h1>
+      <h1 className="form-title">
+        User: {emailField} pass:{passwordField}
+      </h1>
+      <form name="login" className="form">
         <div className="form-field">
-          <input type="text" id="formEmail" placeholder="Input E-mail"/>
+          <input
+            type="text"
+            name="email"
+            id="formEmail"
+            placeholder="Input E-mail"
+            onChange={validateFields}
+          />
           <label htmlFor="formEmail">
-            <IconMail icon="email"/>
+            <IconMail icon="email" />
           </label>
         </div>
         <div className="form-field">
-          <input type="password" id="formPass" placeholder="Input Password"/>
+          <input
+            type="password"
+            name="password1"
+            id="formPass"
+            placeholder="Input Password"
+            onChange={validateFields}
+          />
           <label htmlFor="formPass">
-          <IconMail icon="password"/>
+            <IconMail icon="password" />
           </label>
         </div>
         <div className="form-field">
-          <input type="password" id="formPassConfirm" placeholder="Confirm password"/>
+          <input
+            type="password"
+            name="password2"
+            id="formPassConfirm"
+            placeholder="Confirm password"
+            onChange={validateFields}
+          />
           <label htmlFor="formPassConfirm">
-          <IconMail icon="password"/>
+            <IconMail icon="password" />
           </label>
         </div>
         <div className="form-field">
-          <button className="cta">Create account</button>
+          <button name="btn" className="cta" disabled>
+            Create account
+          </button>
         </div>
       </form>
     </div>
